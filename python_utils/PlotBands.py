bugfix of calculation and output of spin texture

python_utils/PlotBands.py
@@ -96,7 +96,7 @@ def Plot_bandstructure_spin(epsk,spin,klabel,spin_dir=2,Emin=-100.0,Emax=100.0,f
     nbnd = epsk.shape[1]
     xk = np.linspace(0.0, 1.0, nk)
 
-    Sp = spin[:,:,spin_dir]
+    Sp = spin[:,spin_dir,:]
 
     fig, ax = plt.subplots()
 

python_utils/read_wann_calc_txt.py
@@ -28,9 +28,9 @@ def ReadSpin(pref):
     nk = spin_x.shape[0]
     nbnd = spin_x.shape[1]
     spin = np.zeros([nk,nbnd,3])
-    spin[:,:,0] = spin_x
-    spin[:,:,1] = spin_y
-    spin[:,:,2] = spin_z
+    spin[:,0,:] = spin_x
+    spin[:,1,:] = spin_y
+    spin[:,2,:] = spin_z
 
     return spin
 #----------------------------------------------------------------------