dbf4914526be98e068569e0d169d6aa64786c3ca
schuel_m <michael.schueler@psi.ch>
2023-01-25 16:37:29 +0100

    update

--- .configure-custom.sh
+++ .configure-custom.sh
@@ -1,2 +1,2 @@
-ENABLE_VARS="omp|ON|omp| hdf5|ON|hdf5 | mpi|ON|mpi | simd|ON|simd | wannierlib|ON|wannier_lib"
+ENABLE_VARS="omp|ON|omp| hdf5|ON|hdf5 | mpi|ON|mpi | simd|ON|simd | wannierlib|ON|wannier_lib macosx_path|ON|CMAKE_MACOSCX_RPATH"
 WITH_VARS="lapack|path|lapackblas_library scitools|path|scitools_prefix"
