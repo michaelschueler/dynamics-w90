62247b8adc0028d920424cbd5da9ca1da6f3a726
michaelschueler <michael.schueler@psi.ch>
2023-05-19 23:41:49 +0200

    testing

--- src/programs/Mwann_evol_mpi.F90
+++ src/programs/Mwann_evol_mpi.F90
@@ -209,10 +209,10 @@ contains
          call Wann_timestep_RelaxTime_dip(me%ham,me%Nk_loc,me%kcoord_loc,tstp,dt,field,T1,T2,&
             me%Beta,me%MuChem,me%Rhok,method=me%propagator)
       case(dip_emp_gauge)
-         call Wann_timestep_RelaxTime(me%ham,me%Nk_loc,me%kcoord_loc,tstp,dt,field,T1,T2,&
+         call Wann_timestep_RelaxTime_dip(me%ham,me%Nk_loc,me%kcoord_loc,tstp,dt,field,T1,T2,&
             me%Beta,me%MuChem,me%Rhok,empirical=.true.,method=me%propagator)        
       case(velocity_gauge,velo_emp_gauge)
-         call Wann_timestep_RelaxTime(me%nbnd,me%Nk_loc,me%Hk,me%velok,tstp,dt,field,T1,T2,&
+         call Wann_timestep_RelaxTime_velo_calc(me%nbnd,me%Nk_loc,me%Hk,me%velok,tstp,dt,field,T1,T2,&
             me%Beta,me%MuChem,me%Rhok,method=me%propagator)
       end select
 
