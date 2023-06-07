68ddb8af316574a88e60f9dc6a6c4b4162cbfeb4
michaelschueler <michael.schueler@psi.ch>
2023-06-07 11:49:42 +0200

    testing

--- src/programs/Mwann_evol.F90
+++ src/programs/Mwann_evol.F90
@@ -178,6 +178,7 @@ contains
       call batch_diag%Init(me%nbnd,nthreads=nthreads)      
 
       if(me%fft_mode) then
+         print*, shape(me%Hk)
          call me%ham_fft%GetHam(me%Hk)
       else
          call Wann_GenHk(me%ham,me%Nk,me%kcoord,me%Hk)
