5716946df42620518dedcbc5620a03c038caff8f
michaelschueler <michaelschuler.psit@gmail.com>
2024-01-08 21:22:55 +0100

    testing

--- src/pes/pes_scattwf.F90
+++ src/pes/pes_scattwf.F90
@@ -75,7 +75,7 @@ contains
          x = k * r
          if(x < small) then
             Rr = Coulomb_smallx(x, k, eta, l)
-            print*, l, eta, x
+            print*, l, eta, x, Rr
          else
             call COUL90(x, eta, 0.0_dp, l, FC, GC, FCP, GCP, 0, IFAIL)
             Rr = FC(l)  / (r + eps)
